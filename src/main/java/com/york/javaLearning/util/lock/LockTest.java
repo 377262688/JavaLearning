a461af2033157dffda397fa957d8b0ed8c6eefd1
york <377262688@qq.com>
2020-06-14 16:01:19 +0800

    增价AQS相关知识

--- src/main/java/com/york/javaLearning/util/lock/LockTest.java
+++ src/main/java/com/york/javaLearning/util/lock/LockTest.java
@@ -1,5 +1,8 @@
 package com.york.javaLearning.util.lock;
 
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * @author york
  * @create 2020-06-12 16:09
@@ -8,5 +11,6 @@ public class LockTest {
 
     public static void main(String[] args) {
         System.out.println(null == null);
+        ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
     }
 }
